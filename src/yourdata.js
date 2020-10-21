120afb30145938db648105424fe772917ce732a5
josh hall <HallHammR@gmail.com>
2020-10-20 20:40:59 -0400

    port

---
+++ src/yourdata.js
@@ -0,0 +1,69 @@
+export default
+    {
+       
+        name :'Josh Hall',
+        headerTagline: [
+                        'Hi 👋 You have found your new programmer!! My name is Josh Hall.',
+                    
+                        'Animator, programmer, drummer, workhorse',
+                       
+                        'From Richmond VA'
+    ],
+       
+        contactEmail:'HallHammR@gmail.com',
+       
+        abouttext: "My name is Josh Hall. I was a drummer in a professional metal band for most of my early days.  After that I became an animator and animated several music videos and ads for skateboard companies includint: Thrasher Magazine, Creature Skateboards, and Slime Wheels.  This career led me to develope a new passion in programming.",
+        aboutImage:'/assets/886311_554871484544845_108255684_o.jpg',
+       
+       ShowAboutImage:true,
+   
+       projects:[
+           {
+            id: 1,
+            title:'Spirited Scenery', 
+             service:'API project',
+            
+             imageSrc:"assets/Screen Shot 2020-10-20 at 8.00.34 PM.png",
+          
+             url:'https://joshhallrva.github.io/Spirited-Scenery/'
+            },
+            {
+                id: 2,
+                title: 'Password Generator',
+                service: 'Javascript projecdt',
+                imageSrc: "assets/Screen Shot 2020-10-20 at 8.11.16 PM.png",
+                url: 'https://joshhallrva.github.io/Password-Generator/'
+            },
+            { 
+                id: 3,
+                title: 'Weather API',
+                service: 'Weather gathering API app',
+                imageSrc: "assets/Screen Shot 2020-10-20 at 8.14.12 PM.png",
+                url: 'https://joshhallrva.github.io/Weather-app/'
+            },
+            {
+                id: 4,
+                title: 'Budget tracker',
+                service: 'locally hosted budget graph generator',
+                imageSrc: "assets/Screen Shot 2020-10-20 at 8.22.04 PM.png",
+                url: 'https://sleepy-shelf-43861.herokuapp.com/'
+           }
+
+       
+        ],
+        social: [
+           
+            {   name:'Github',
+                url:'https://github.com/JoshHallRVA'},
+            
+            {
+                name: 'Other Portfolio site',
+                url: 'https://joshhallrva.github.io/Updated-portfolio/'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/hallhammr/'
+            }
+
+        ]
+    }
